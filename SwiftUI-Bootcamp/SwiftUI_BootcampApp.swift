32aa53fb6bfa7e69f4cdebfbefbca6d9008b0dfb
mulgir-govind <govind.mulgir.17@gmail.com>
2024-08-24 18:41:55 +0530

    Initial Commit

---
+++ SwiftUI-Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Govind on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
