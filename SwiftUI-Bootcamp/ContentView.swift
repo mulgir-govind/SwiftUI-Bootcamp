13b53727fad9c7317794fdec43dedd8a10505284
mulgir-govind <govind.mulgir.17@gmail.com>
2024-08-29 18:26:21 +0530

    [BeginnerLevel] Show example of adding padding on views
    - Added different examples showing how can we set padding on all/different sides

--- SwiftUI-Bootcamp/ContentView.swift
+++ SwiftUI-Bootcamp/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            StacksExample()
+        VStack(spacing: 0) {
+            PaddingExample()
         }
         .padding()
     }
